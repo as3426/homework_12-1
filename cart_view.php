f90c4cbaf665cd0cc22c620259e0d2ea7b09f11d
A Sharma <as3426@njit.edu>
2017-07-23 10:23:01 -0400

    display session id

--- cart_view.php
+++ cart_view.php
@@ -1,4 +1,4 @@
-<!DOCTYP">
+<!DOCTYPE html">
 <htm">
 <head>
     <title>My Guitar Shop</title>
@@ -61,6 +61,9 @@
         <?php endif; ?>
         <p><a href=".?action=show_add_item">Add Item</a></p>
         <p><a href=".?action=empty_cart">Empty Cart</a></p>
+
+
+ <p>Session ID: <?php echo session_id(); ?></p>
 
     </main>
 </body>
